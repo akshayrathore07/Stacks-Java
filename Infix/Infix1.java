952c8479ab5c5d3ad40572f909fa4cf86f02baac
Akshay Kumar <110248720+akshayrathore07@users.noreply.github.com>
2023-08-30 15:32:17 +0530

    Add files via upload

---
+++ Infix/Infix1.java
@@ -0,0 +1,90 @@
+package Infix;
+
+import java.util.Stack;
+
+public class Infix1 {
+    public static void main(String[] args) {
+        String str = "8-5+3*4/6";
+        Stack<Integer> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for (int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            int ascii = (int) ch;
+            // ascii value of 0 is 48 and '9' --> 57
+            if (ascii >= 48 && ascii<=57){
+                val.push(ascii-48);
+            }
+            else if(op.size()==0){
+                op.push(ch);
+            }
+            else{
+                if (ch== '+' || ch == '-'){
+                    // work
+                    int v2 = val.pop();
+                    int v1 = val.pop();
+
+                    if (op.peek() == '-'){
+                        val.push(v1-v2);
+                    }
+                    if (op.peek() == '+'){
+                        val.push(v1+v2);
+                    }
+                    if (op.peek() == '*'){
+                        val.push(v1*v2);
+                    }
+                    if (op.peek() == '/'){
+                        val.push(v1/v2);
+                    }
+
+                    op.pop();
+                    // push
+                    op.push(ch);
+                }
+                if (ch == '*' || ch == '/'){
+                    if (op.peek() == '*' || op.peek() == '/'){
+                        // work
+                        int v2 = val.pop();
+                        int v1 = val.pop();
+
+                        if (op.peek() == '*'){
+                            val.push(v1*v2);
+                        }
+                        if (op.peek() == '/'){
+                            val.push(v1/v2);
+                        }
+
+                        op.pop();
+                        // push
+                        op.push(ch);
+                    }
+                    else {
+                        op.push(ch);
+                    }
+                }
+            }
+        }
+        // val stack size -> 1
+        while (val.size() >1){
+            int v2 = val.pop();
+            int v1 = val.pop();
+
+            if (op.peek() == '-'){
+                val.push(v1-v2);
+            }
+            if (op.peek() == '+'){
+                val.push(v1+v2);
+            }
+            if (op.peek() == '*'){
+                val.push(v1*v2);
+            }
+            if (op.peek() == '/'){
+                val.push(v1/v2);
+            }
+            op.pop();
+
+        }
+        System.out.println(val.peek());
+    }
+}
+
